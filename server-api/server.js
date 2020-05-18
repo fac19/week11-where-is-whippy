feba551dbdd68a6edde001717446986fd2e2eca6
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-05-18 15:46:55 +0100

    Add pseudocode routes to API.
    Co-authored-by: itsina96 <itsina96@gmail.com>
    Co-authored-by: lizzy-j <lizzyhj@hotmail.com>
    Co-authored-by: joepock123 <joe.m.jackson@live.co.uk>

---
+++ server-api/server.js
@@ -0,0 +1,31 @@
+// Tables
+// Customers Info
+// Vendors Info
+// Customer location
+// vendor live location
+// vendor routes
+
+// routes
+// Vendors need:
+// Get login information
+// Post login information
+
+// Get customer locations
+
+// Post routes
+// Get routes
+// Update routes
+// Delete routes
+
+// post live location
+// update live location
+// get live location
+
+// customer needs:
+// post login
+// get login
+
+// get vendor live location
+// get vendor routes
+
+// post customer location
