611f59dc12930577e4884ec30edcae0b43ea073d
Ina Yoon <itsina96@gmail.com>
2020-05-19 16:18:33 +0100

    Update react router in app.js
    Co-authored-by: joepock123 <joe.m.jackson@live.co.uk>

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,21 +2,48 @@ import React, { useState } from "react"
 import "./App.css"
 import Landing from "./components/shared/Landing.js"
 import Header from "./components/shared/Header.js"
+import Login from "./components/shared/Login.js"
 // For react router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   const [logInStatus, setLogInStatus] = useState(false)
+  const [isVendor, setIsVendor] = useState(true)
+  console.log("App -> isVendor", isVendor)
 
   return (
     <Router>
       <div className="App">
         <Header logInStatus={logInStatus} />
-        {/* <Landing /> */}
-        <Route path="/landing" component={Landing} />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={() => <Landing setIsVendor={setIsVendor} />}
+          />
+          <Route
+            path="/login"
+            component={() => <Login isVendor={isVendor} />}
+          />
+        </Switch>
       </div>
     </Router>
   )
 }
 
+/* <Route exact path="/props-through-render" render={(props) => <PropsPage {...props} title={`Props through render`} />} />
+
+<Route
+  path='/dashboard'
+  component={() => <Dashboard isAuthed={true} />}
+/> */
+
+/* <Route
+  path='/dashboard'
+  render={(props) => <Dashboard {...props} isAuthed={true} />}
+/> */
+
 export default App
+
+/* <Route path="/signup-c" component={signupCustomer} />
+<Route path="/signup-v" component={signupVendor} /> */
---
+++ client-app/src/components/shared/login.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function login(isVendor) {
+  return (
+    <section>
+      {isVendor ? (
+        <h2 className="login-subtitle">Find ice cream lovers today</h2>
+      ) : (
+        <h2 className="login-subtitle">Find ice cream today</h2>
+      )}
+    </section>
+  )
+}
+
+// <h2 className="login-subtitle">Find</h2>Find
