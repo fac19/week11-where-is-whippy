44eb082e21ec6669102c6f21580b6b1d354b8eed
Ina Yoon <itsina96@gmail.com>
2020-05-19 13:57:46 +0100

    Add react router in app.js

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from "react"
 import "./App.css"
 import Landing from "./components/shared/Landing.js"
 import Header from "./components/shared/Header.js"
+// For react router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   const [logInStatus, setLogInStatus] = useState(false)
 
   return (
-    <div className="App">
-      <Header logInStatus={logInStatus} />
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Header logInStatus={logInStatus} />
+        {/* <Landing /> */}
+        <Route path="/landing" component={Landing} />
+      </div>
+    </Router>
   )
 }
 
