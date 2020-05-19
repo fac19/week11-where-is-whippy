aaf2e57a41573b119ab7a6754bb58db10bc9b289
Ina Yoon <itsina96@gmail.com>
2020-05-19 12:35:03 +0100

    Create header components
    Co-authored-by: joepock123 <joe.m.jackson@live.co.uk>

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
+import Landing from "./components/shared/Landing.js"
+import HeaderLoggedOut from "./components/shared/HeaderLoggedOut.js"
 
 function App() {
+  const [logInStatus, setLogInStatus] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Wheres Whippy</h1>
+
+        {!logInStatus && <HeaderLoggedOut />}
       </header>
+      <Landing />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
