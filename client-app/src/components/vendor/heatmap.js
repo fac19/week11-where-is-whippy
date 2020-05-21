2a22268a0e00d3b100d223f083bec3ff158fa2fc
Lizzy-j <lizzyhj@hotmail.com>
2020-05-21 15:57:55 +0100

    Fix bug in google map and dotenv file Relates #15
    Co-authored-by: HettieM <hettie.mcconnell@outlook.com>

--- client-app/src/components/vendor/heatmap.js
+++ client-app/src/components/vendor/heatmap.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import LondonMap from "../../utils/london-map.js"
 
@@ -6,11 +6,25 @@ export default function Heatmap() {
   //fetch API - retrieve all lat/lng coordinates from
   //then(data => data.forEach(create new array heatmapData))
 
+  //   const [customerPosition, getCustomerPositions] = React.useState({})
+  //   const marker = (position) => {
+  //     const position = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude,
+  //     } //marker should be an object made up the arrays pulled from the fetch request?
+  //     getCustomerPositions(customerPosition)
+  //   }
+
+  //   React.useEffect(() => {
+  //     navigator.geolocation.getCustomerPosition(marker)
+  //   })
+
+  // where does code for the marker go? to be imported or exported?
+
   return (
     <section>
       <section className="map">
         <LondonMap />
-        <p>hello</p>
       </section>
       <Link
         to={{
--- client-app/src/utils/london-map.js
+++ client-app/src/utils/london-map.js
@@ -16,7 +16,6 @@ export default function LondonMap() {
 
   return (
     <section>
-      <p> London Map Function </p>
       <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
         <GoogleMap
           mapContainerStyle={mapStyles}
@@ -27,37 +26,3 @@ export default function LondonMap() {
     </section>
   )
 }
-
-// var script = document.createElement('script');
-// script.src = "https://maps.googleapis.com/maps/api/js?key=${YOUR_API_KEY}&libraries=visualization";
-// script.defer = true;
-// script.async = true;
-
-// let map;
-
-// function initMap() {
-//     map = new google.maps.Map(document.querySelector(".map"), {
-//         center: {lat: , lng: },
-//         zoom: 10
-//     });
-// }
-
-// window.initMap;
-
-// OR
-
-// let london = new google.maps.LatLng(lat, lng)
-
-// map = new google.maps.Map(document.querySelector('.map'), {
-//     center: london,
-//     zoom: 13,
-//     mapTypeId: 'satellite'
-// })
-
-// https://developers.google.com/maps/documentation/javascript/tutorial
-
-// https://dev.to/jessicabetts/how-to-use-google-maps-api-and-react-js-26c2
-
-// https://janosh.io/blog/google-maps+react-hooks (!!!)
-
-// * https://medium.com/@allynak/how-to-use-google-map-api-in-react-app-edb59f64ac9d// * https://medium.com/@allynak/how-to-use-google-map-api-in-react-app-edb59f64ac9d
