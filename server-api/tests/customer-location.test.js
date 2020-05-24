87d1d20c41c1242438f5bb4fab6a444a1562f8ee
Joe Jackson <joe.m.jackson@live.co.uk>
2020-05-24 18:41:34 +0100

    Complete db testing for cus locations
    Hating on jest so much for testing the handler

--- server-api/tests/customer-location.test.js
+++ server-api/tests/customer-location.test.js
@@ -1,14 +1,18 @@
 const build = require("../db/build")
 const db = require("../db/connection")
-const server = require("../server")
-const supertest = require('supertest')
-const request = supertest(server)
 
 const model = require("../model/customer-location-m")
 const handler = require("../handlers/customer-location-h")
 
-afterAll(() => {
-  db.end()
+const supertest = require("supertest")
+const server = require("../server")
+
+// *important* Without this you get the error:
+// "This usually means that there are asynchronous operations that weren't stopped in your tests."
+afterAll(done => {
+  db.end() // After all the tests have run close the db connection
+  server.close()
+  done()
 })
 
 test("Tests are working", () => {
@@ -17,24 +21,47 @@ test("Tests are working", () => {
   expect(actual).toBe(expected)
 })
 
-describe("Test the customer location model", () => {
-  beforeAll(() => {
-    return build() // Why do we need to return build again?
+describe("Test the customer location model db queries", () => {
+  beforeEach(() => {
+    return build() // We teardown the db (drop the tables )then initialize it before each test
   })
 
-  test("Can GET all customer locations", () => {
-    return model
-      .getAllCustomerLocations() // Make sure you return something otherwise the tests will not run
+  // Declare the variables to be used in the tests
+  let customerId
+  let lat
+  let lng
+  let temp
+
+  test("1) Using promises: Can GET all customer locations", () => {
+    expect.assertions(4) // We want to make sure the promise resolves and the three tests are run
+    return model // Make sure you return something otherwise the tests will not run
+      .getAllCustomerLocations()
       .then((locations) => {
         expect(locations.length).toBe(5)
         expect(typeof locations[2].latitude).toBe(typeof "string")
         expect(locations[2].latitude).toBe("51.500729")
+        expect(locations[2]).toHaveProperty("latitude", "51.500729")
       })
   })
 
+  test("2) Using .resolves: Can GET all customer locations", () => {
+    expect.assertions(1)
+    return expect(model.getAllCustomerLocations()).resolves.toHaveLength(5)
+  })
+
+  test("3) Using Async/Await: Can GET all customer locations", async () => {
+    expect.assertions(1)
+    const locations = await model.getAllCustomerLocations()
+    expect(locations.length).toBe(5)
+  })
+
   test("Can POST new customer location entry", () => {
+    customerId = 1
+    lat = 51.496281
+    lng = 0.1452
+    temp = 21
     return model
-      .addNewCustomerLocation(1, 51.496281, 0.1452, 21)
+      .addNewCustomerLocation(customerId, lat, lng, temp)
       .then((result) => {
         expect(typeof result.rows[0].latitude).toBe(typeof "51.496281")
         expect(result.rows[0].latitude).toBe("51.496281")
@@ -43,17 +70,21 @@ describe("Test the customer location model", () => {
   })
 })
 
-describe("Test the customer location handler", () => {
-  beforeAll( () => {
-    return build() 
+// test("Simulate what supertest is doing", () => {
+//   const fakeReq = {}
+//   const fakeRes = { send: (data) => expect(data.length).toBe(5) }
+//   return handler.allCustomerLocations(fakeRequest, fakeResponse)
+// })
+
+describe("Test the customer location handler and endpoints", () => {
+  beforeEach(() => {
+    return build()
   })
 
   test("GET all customer locations", async () => {
-    const res = await request.get('/customers/coords')
-    expect(res.status).toBe(201)
+    const res = await supertest(server).get("/customers/coords")
+    expect(res.status).toBe(200)
   })
-
-  
 })
 
 // test("Ensure customer cannot create a new database entry within half an hour of their last", () => {
