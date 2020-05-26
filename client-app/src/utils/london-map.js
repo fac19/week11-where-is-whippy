e93d0ddbaf25fee7d7007cf568baa4d13d735117
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-05-26 14:35:03 +0100

    Small changes

--- client-app/src/utils/london-map.js
+++ client-app/src/utils/london-map.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import { Map, GoogleApiWrapper } from "google-maps-react"
 import { GoogleMap, LoadScript } from "@react-google-maps/api"
 require("dotenv").config()
 
@@ -26,3 +25,29 @@ export default function LondonMap() {
     </section>
   )
 }
+
+// import { Map, GoogleApiWrapper } from 'google-maps-react'
+
+// const mapStyles = {
+//   height: 500,
+//   width: 500,
+// }
+
+// const defaultCenter = {
+//   lat: 51.5074,
+//   lng: 0.1278,
+// }
+
+// render() {
+//     return (
+//       <Map
+//       google={this.PaymentResponse.google}
+//       zoom={8}
+//       style={mapsStyles}
+//       initialCenter={defaultCenter} />
+//   );
+// }
+
+// export default GoogleApiWrapper( {
+//   apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+// })
