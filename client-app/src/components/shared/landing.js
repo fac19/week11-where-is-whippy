9f26fe01039e04548cc25f11abb049231c61d62c
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-05-27 12:09:19 +0100

    Styled buttons and basic outline for pages.
    Co-authored-by: lizzy-j <lizzyhj@hotmail.com>

--- client-app/src/components/shared/landing.js
+++ client-app/src/components/shared/landing.js
@@ -2,33 +2,35 @@
 import React, { useContext } from "react"
 import { Link } from "react-router-dom"
 import { AppContext } from "../AppContext"
+import { BlueButton, PinkButton, PageContainer } from "../../styles/buttons.js"
 
 export default function Landing() {
   const { isVendor, setIsVendor } = useContext(AppContext)
 
   return (
-    <section>
+    <PageContainer>
       <h2 className="landing-subtitle">Tired of waiting for the jingle?</h2>
       <img
         className="landing-img"
         src="https://media.giphy.com/media/3orieXuD6SY02z1VtK/giphy.gif"
       />
-      <Link
-        to="/user"
+
+      <PinkButton
+        href="/user"
         onClick={() => setIsVendor(true)}
         className="landing-btn-vendors"
       >
         Vendors
-      </Link>
+      </PinkButton>
 
-      <Link
-        to="/user"
+      <BlueButton
+        href="/user"
         onClick={() => setIsVendor(false)}
         className="landing-btn-customers"
       >
         Customers
-      </Link>
-    </section>
+      </BlueButton>
+    </PageContainer>
   )
 
   // function handleLoginLinkVendor() {
---
+++ client-app/src/styles/buttons.js
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+
+const lightBlue = "#C3DDED"
+const navy = "#1f4068"
+const lightPink = "#F7DCD5"
+const background = "#F7F4F0"
+
+const Button = `
+padding: 10% 15% 10% 15%;
+margin: 5% 5% 0 5%;
+  display: block;
+  max-width: 200px;
+  border-radius: 5%;
+  color: ${navy};
+  text-decoration: none;
+  font-size: 2rem;
+`
+
+const BlueButton = styled.a`
+${Button}
+background-color: ${lightBlue}
+`
+
+const PinkButton = styled.a`
+  ${Button}
+  background-color: ${lightPink};
+`
+
+const PageContainer = styled.section`
+  background-color: ${background};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  min-height: 90vh;
+`
+
+const Header = styled.h1`
+  width: 320px;
+  height: 80px;
+  font-style: italic;
+  color: navy;
+}
+`
+
+export { BlueButton, PinkButton, PageContainer, Header }
