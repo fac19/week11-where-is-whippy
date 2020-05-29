723ee49d35130fd7e1bcec16e464d27ecc9d9744
Ina Yoon <itsina96@gmail.com>
2020-05-29 09:03:33 +0100

    Change frontend link in server.js

--- server-api/server.js
+++ server-api/server.js
@@ -1,66 +1,66 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
 // Handler modules
-const customerLocationsHandler = require("./handlers/customer-location-h")
-const vendorLocationsHandler = require("./handlers/vendor-location-h")
-const vendorRoutesHandler = require("./handlers/vendor-routes-h")
-const vendors = require("./handlers/vendors-h")
-const customers = require("./handlers/customers-h")
+const customerLocationsHandler = require("./handlers/customer-location-h");
+const vendorLocationsHandler = require("./handlers/vendor-location-h");
+const vendorRoutesHandler = require("./handlers/vendor-routes-h");
+const vendors = require("./handlers/vendors-h");
+const customers = require("./handlers/customers-h");
 
 // Middleware
-const handleError = require("./middleware/handleError")
-const logger = require("./middleware/logger")
-const auth = require("./middleware/auth")
+const handleError = require("./middleware/handleError");
+const logger = require("./middleware/logger");
+const auth = require("./middleware/auth");
 
-const server = express()
-server.use(express.json())
-server.use(logger)
+const server = express();
+server.use(express.json());
+server.use(logger);
 
 // Only allowing cross origin request from a specific url.
 const corsOptions = {
   origin:
     process.env.NODE_ENV == "production"
-      ? "https://competent-feynman-176d4a.netlify.app/"
+      ? "https://where-is-whippy.netlify.app/"
       : "http://localhost:3000",
-}
+};
 
 // Request Handling
-server.use(cors(corsOptions))
+server.use(cors(corsOptions));
 
 //GET
 
 // REST API
 // GETauth. verifyCustomer,
-server.get("/customers", customers.allCustomers) // WORKING
-server.get("/customers/coords", customerLocationsHandler.allCustomerLocations) // WORKING
-server.get("/customers/:id", customers.getSpecificCustomer) // WORKING
-server.get("/vendors", vendors.allVendors) // WORKING
-server.get("/vendors/coords", vendorLocationsHandler.allVendorLocations) // WORKING
-server.get("/vendors/:id", vendors.getSpecificVendor) // WORKING
-server.get("/vendors/routes/:name", vendorRoutesHandler.getRoute) // WORKING
+server.get("/customers", customers.allCustomers); // WORKING
+server.get("/customers/coords", customerLocationsHandler.allCustomerLocations); // WORKING
+server.get("/customers/:id", customers.getSpecificCustomer); // WORKING
+server.get("/vendors", vendors.allVendors); // WORKING
+server.get("/vendors/coords", vendorLocationsHandler.allVendorLocations); // WORKING
+server.get("/vendors/:id", vendors.getSpecificVendor); // WORKING
+server.get("/vendors/routes/:name", vendorRoutesHandler.getRoute); // WORKING
 
 // POST
-server.post("/customers/signup", customers.createCustomer) // WORKING
-server.post("/customers/login", customers.loginCustomer) // WORKING
+server.post("/customers/signup", customers.createCustomer); // WORKING
+server.post("/customers/login", customers.loginCustomer); // WORKING
 server.post(
   "/customers/coords/",
   auth.verifyCustomer,
   customerLocationsHandler.addNewCustomerLocation
-) // WORKING
+); // WORKING
 
-server.post("/vendors/signup", vendors.createVendor) // WORKING
-server.post("/vendors/login", vendors.loginVendor) // WORKING
+server.post("/vendors/signup", vendors.createVendor); // WORKING
+server.post("/vendors/login", vendors.loginVendor); // WORKING
 server.post(
   "/vendors/coords/",
   auth.verifyVendor,
   vendorLocationsHandler.addNewVendorLocation
-) // WORKING
+); // WORKING
 server.post(
   "/vendors/routes/",
   auth.verifyVendor,
   vendorRoutesHandler.createNewRoute
-) // WORKING
+); // WORKING
 
 // PUT
 // server.put('/vendor endpoint', callback) // For vendors to update info
@@ -68,8 +68,8 @@ server.post(
 // DELETE
 // server.delete('/vendor endpoint', callback) // For vendors to delete account
 // server.delete('/customer endpoint', callback) // For customer to delete account
-server.delete("/vendors/routes/:name", vendorRoutesHandler.deleteRoute)
+server.delete("/vendors/routes/:name", vendorRoutesHandler.deleteRoute);
 
-server.use(handleError)
+server.use(handleError);
 
-module.exports = server
+module.exports = server;
